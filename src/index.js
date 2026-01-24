c2e9aa27d8717ac5f4abe198e5bf96dc17313d2e
Aryan Jain <jainaryan1818@gmail.com>
2026-01-24 14:09:55 +0530

    added token functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-import mongoose from "mongoose";
+const MAX_TOKENS = 20;
+const REFILL_RATE = 50000; // 5 seconds
 
-function connectDB (){}
+const bucket = {
+    tokens: MAX_TOKENS,
+    hasTokens: function () {
+        return this.tokens > 0;
+    },
+    consumeToken: function () {
+        if (this.hasTokens())
+            this.tokens -= 1;
+    },
+    releaseToken: function () {
+        if (this.tokens < MAX_TOKENS)
+            this.tokens += 1;
+    }
+};
 
-connectDB();
+async function handleIncomingRequest(requestId) {
+    if (!bucket.hasTokens()) {
+        console.log('❌ Out of tokens! Please try again later', requestId);
+        return;
+    }
+
+    bucket.consumeToken();
+    console.log('✅ Processing Request...', requestId, '| Tokens left:', bucket.tokens);
+
+    await waitFor(2000);
+}
+
+function waitFor(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Refill token every 5 seconds
+setInterval(() => {
+    bucket.releaseToken();
+    console.log('🔁 Token refilled | Tokens:', bucket.tokens);
+}, REFILL_RATE);
+
+// Simulate incoming requests
+let requestId = 1;
+setInterval(() => {
+    handleIncomingRequest(requestId++);
+}, 1000);
